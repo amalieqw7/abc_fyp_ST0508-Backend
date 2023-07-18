a497779e90c8b58594c7cf3dc9d3d8a09c3c5aa2
amalieqw7 <amalieqw.21@ichat.sp.edu.sg>
2023-07-18 18:40:46 +0800

    #17 - Get ADHOC by PRID API

--- routes/idvRoutes/purchaseReqRoutes.js
+++ routes/idvRoutes/purchaseReqRoutes.js
@@ -20,6 +20,7 @@ router.put('/PR/ApprComment/:id', purchaseReqController.updateApprComments);
 router.delete('/PR/:id', purchaseReqController.deletePRById);
 router.get('/adhoc/purchases', purchaseReqController.getAllAdHoc);
 router.get('/adhoc/:id', purchaseReqController.getAdHocByUserID);
+router.get('/adhoc/viewBy/:id', purchaseReqController.getAdHocByPRID);
 
 // Line Item
 router.post('/lineItem', purchaseReqController.addLineItem);
